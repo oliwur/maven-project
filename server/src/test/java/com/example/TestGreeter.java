05e3750f35499bf9c644199cd74e8bd1bdafc0f8
oli <oli.wurster@gmail.com>
2019-04-02 00:32:08 +0200

    remove the always fail()ing test.

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -31,9 +31,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-  @Test
-  public void thisWillAlwaysFail() {
-    fail("shit happens.");
-  }
 }
